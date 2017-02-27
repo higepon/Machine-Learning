422e61ab245706cfedf3812d29b805b6792cbe8d
Taro Minowa <higepon@gmail.com>
2017-02-27 20:57:23 +0900

    trying to predict nikkei data

---
+++ nikkei.py
@@ -0,0 +1,107 @@
+# From http://qiita.com/yukiB/items/5d5b202af86e3c587843
+
+# try:
+#     # pydot-ng is a fork of pydot that is better maintained
+#     import pydot_ng as pydot
+# except ImportError:
+#     # fall back on pydot if necessary
+#     import pydot
+# if not pydot.find_graphviz():
+#     raise RuntimeError('Failed to import pydot. You must install pydot'
+#                        ' and graphviz for `pydotprint` to work.')
+import sys
+import os
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+from keras.utils.visualize_util import plot
+from matplotlib.pyplot import show, plot
+import pandas as pd
+import numpy as np
+import math
+import random
+random.seed(0)
+
+version = 2
+weights_dir = "/Users/higepon/Desktop/{0}-{1}".format(sys.argv[0], version)
+
+def train_test_split(df, test_size=0.1, n_prev = 100):  
+    """
+    This just splits data to training and testing parts
+    """
+    ntrn = round(len(df) * (1 - test_size))
+    ntrn = int(ntrn)
+    X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
+    X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
+
+    return (X_train, y_train), (X_test, y_test)
+
+def _load_data(data, n_prev = 100):  
+    """
+    data should be pd.DataFrame()
+    """
+
+    docX, docY = [], []
+    for i in range(len(data)-n_prev):
+        docX.append(data.iloc[i:i+n_prev].as_matrix())
+        docY.append(data.iloc[i+n_prev].as_matrix())
+    alsX = np.array(docX)
+    alsY = np.array(docY)
+
+    return alsX, alsY
+
+def train():
+    model, _, _, X_train, y_train = create_model()
+    os.makedirs(weights_dir, exist_ok=True)
+    filepath = weights_dir + "/{loss:.4f}"
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+    callbacks_list = [checkpoint]
+    model.fit(X_train, y_train, batch_size=600, nb_epoch=15, validation_split=0.05, callbacks=callbacks_list)
+
+def create_model():    
+    df = pd.read_csv("nikkei.csv", header=None)
+    df[1] = df[1].apply(lambda x: x / 10000)
+    df[1] = df[1].astype('float32')
+    X = df[[1]]
+    length_of_sequences = 100
+    (X_train, y_train), (X_test, y_test) = train_test_split(X, n_prev=length_of_sequences)
+    
+    in_out_neurons = 1
+    hidden_neurons = 300
+    model = Sequential()  
+    model.add(LSTM(hidden_neurons, batch_input_shape=(None, length_of_sequences, in_out_neurons), return_sequences=False))  
+    model.add(Dense(in_out_neurons))  
+    model.add(Activation("linear"))  
+    model.compile(loss="mean_squared_error", optimizer="rmsprop")
+    return (model, X_test, y_test, X_train, y_train)
+
+def best_model_path():
+    files = os.listdir(weights_dir)
+    files.sort()
+    return "{0}/{1}".format(weights_dir, files[0])
+
+def predict():
+    model, X_test, y_test, _, _ = create_model()
+    model.load_weights(best_model_path())    
+    predicted = model.predict(X_test)
+    dataf =  pd.DataFrame(predicted)
+    dataf.columns = ["predict"]
+    dataf["input"] = y_test
+    dataf.plot(figsize=(15, 5))
+    show()    
+    
+if len(sys.argv) == 2:
+    if sys.argv[1] == "--train":
+        train()
+    elif sys.argv[1] == "--predict":
+        predict()
+    else:
+        print("specify --train or --predict")
+else:
+        print("specify --train or --predict")
+
+exit(-1)
--- sin.py
+++ sin.py
@@ -34,6 +34,8 @@ number_of_cycles = 50
 
 df = pd.DataFrame(np.arange(steps_per_cycle * number_of_cycles + 1), columns=["t"])
 df["sin_t"] = df.t.apply(lambda x: math.sin(x * (2 * math.pi / steps_per_cycle)+ random.uniform(-1.0, +1.0) * random_factor))
+
+# first 160 data
 df[["sin_t"]].head(steps_per_cycle * 2).plot()
 show()
 
@@ -62,59 +64,10 @@ def train_test_split(df, test_size=0.1, n_prev = 100):
 
     return (X_train, y_train), (X_test, y_test)
 
-def _load_data(data, n_prev = 100):  
-    """
-    data should be pd.DataFrame()
-    """
-
-    docX, docY = [], []
-    for i in range(len(data)-n_prev):
-        docX.append(data.iloc[i:i+n_prev].as_matrix())
-        docY.append(data.iloc[i+n_prev].as_matrix())
-    alsX = np.array(docX)
-    alsY = np.array(docY)
-
-    return alsX, alsY
-
-def train_test_split(df, test_size=0.1, n_prev = 100):  
-    """
-    This just splits data to training and testing parts
-    """
-    ntrn = round(len(df) * (1 - test_size))
-    ntrn = int(ntrn)
-    X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
-    X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
-
-    return (X_train, y_train), (X_test, y_test)
-
-def _load_data(data, n_prev = 100):  
-    """
-    data should be pd.DataFrame()
-    """
-
-    docX, docY = [], []
-    for i in range(len(data)-n_prev):
-        docX.append(data.iloc[i:i+n_prev].as_matrix())
-        docY.append(data.iloc[i+n_prev].as_matrix())
-    alsX = np.array(docX)
-    alsY = np.array(docY)
-
-    return alsX, alsY
-
-def train_test_split(df, test_size=0.1, n_prev = 100):  
-    """
-    This just splits data to training and testing parts
-    """
-    ntrn = round(len(df) * (1 - test_size))
-    ntrn = int(ntrn)
-    X_train, y_train = _load_data(df.iloc[0:ntrn], n_prev)
-    X_test, y_test = _load_data(df.iloc[ntrn:], n_prev)
-
-    return (X_train, y_train), (X_test, y_test)
 
 length_of_sequences = 100
 (X_train, y_train), (X_test, y_test) = train_test_split(df[["sin_t"]], n_prev =length_of_sequences)
-
+print(X_train.shape)
 in_out_neurons = 1
 hidden_neurons = 300
 
@@ -123,7 +76,7 @@ model.add(LSTM(hidden_neurons, batch_input_shape=(None, length_of_sequences, in_
 model.add(Dense(in_out_neurons))  
 model.add(Activation("linear"))  
 model.compile(loss="mean_squared_error", optimizer="rmsprop")
-model.fit(X_train, y_train, batch_size=600, nb_epoch=15, validation_split=0.05)
+model.fit(X_train, y_train, batch_size=600, nb_epoch=2, validation_split=0.05)
 
 
 predicted = model.predict(X_test) 
