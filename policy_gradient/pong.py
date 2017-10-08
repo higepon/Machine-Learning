889ccbac99e86a780facffb79e48a21f6049412c
Taro Minowa <higepon@gmail.com>
2017-10-08 15:26:38 +0900

    build_graph

--- policy_gradient/pong.py
+++ policy_gradient/pong.py
@@ -12,73 +12,74 @@ gamma = 0.99
 class PolicyGradientAgent:
     def __init__(self, sess):
         with tf.name_scope("PolicyGradientAgent"):
-            self.__sess = sess
-            weight_init = tf.random_normal_initializer(mean=0.0, stddev=0.05)
+            self._sess = sess
+            self._X, self._log_probs, self._actions, self._advantages, self._loss, self._loss_summary, self._train = self._build_graph()
 
-            self.__X = tf.placeholder(tf.float32, [image_size, None], name="X")
-            # todo: self.W1 necessary?
-            self.W1 = tf.get_variable(name="W1", shape=[num_hidden_layer_size, image_size], initializer=weight_init)
-            W2 = tf.get_variable(name="W2", shape=[1, num_hidden_layer_size], initializer=weight_init)
+    @staticmethod
+    def _build_graph():
+        X = tf.placeholder(tf.float32, [image_size, None], name="X")
+        weight_init = tf.random_normal_initializer(mean=0.0, stddev=0.05)
 
-            # activation of hidden layer
-            Z1 = tf.matmul(self.W1, self.__X)
-            tf.assert_equal(tf.shape(Z1)[0], num_hidden_layer_size)
+        # activation of hidden layer
+        W1 = tf.get_variable(name="W1", shape=[num_hidden_layer_size, image_size], initializer=weight_init)
+        Z1 = tf.matmul(W1, X)
+        tf.assert_equal(tf.shape(Z1)[0], num_hidden_layer_size)
 
-            A1 = tf.nn.relu(Z1)
-            tf.assert_equal(tf.shape(A1)[0], num_hidden_layer_size)
+        A1 = tf.nn.relu(Z1)
+        tf.assert_equal(tf.shape(A1)[0], num_hidden_layer_size)
 
-            # activation of output layer
-            Z2 = tf.matmul(W2, A1)
-            tf.assert_equal(tf.shape(Z2)[0], 1)
+        # activation of output layer
+        W2 = tf.get_variable(name="W2", shape=[1, num_hidden_layer_size], initializer=weight_init)
+        Z2 = tf.matmul(W2, A1)
+        tf.assert_equal(tf.shape(Z2)[0], 1)
 
-            A2 = tf.nn.sigmoid(Z2)
-            tf.assert_equal(tf.shape(A2)[0], 1)
-            self.__probs = A2
+        A2 = tf.nn.sigmoid(Z2)
+        tf.assert_equal(tf.shape(A2)[0], 1)
 
+        # we had log(0) here, so not using log. And also the original script is not using log for some reason.
+        log_probs = A2
 
-            ## log_prob for each input
-            ## shape=[1, batch_size]
-            ## we had log(0) here, so not using log. And also the original script is not using log for some reason.
-            self.__log_probs = A2 # tf.log(A2)
+        # reward for each action take for input
+        # shape=[1, batch_size]
+        advantages = tf.placeholder(tf.float32, shape=[1, None], name="advantages")
 
-            ## reward for each action take for input
-            ## shape=[1, batch_size]
-            self.__advantages = tf.placeholder(tf.float32, shape=[1, None], name="advantages")
+        # to use subtract actions should have taken
+        actions = tf.placeholder(tf.float32, shape=[1, None], name="actions")
+        fake_labels = tf.to_float(tf.equal(actions, 2), name="fake_labels")
 
-            ## to use subtract actions should have taken
-            self.__actions = tf.placeholder(tf.float32, shape=[1, None], name="actions")
-            fake_labels = tf.to_float(tf.equal(self.__actions, 2), name="fake_labels")
+        # We chose action based on sampled probability p(y|x) and random dice.
+        # When we chose to take opposite action to p(y|x), we should take into account that.
+        addjusted_logprob = fake_labels - log_probs
 
-            # We chose action based on sampled probability p(y|x) and random dice.
-            # When we chose to take opposite action to p(y|x), we should take into account that.
-            addjusted_logprob = fake_labels - self.__log_probs
+        # We negate objective function as higher reward is better
+        cost_function = -addjusted_logprob * advantages
 
-            # We negate objective function as higher reward is better
-            self.__hoge = -addjusted_logprob * self.__advantages
+        # todo: confirm if reduce_sum is right way here.
+        loss = tf.reduce_sum(cost_function)
 
-            # todo: confirm if reduce_sum is right way here.
-            self.__loss = tf.reduce_sum(self.__hoge)
+        loss_summary = tf.summary.scalar("loss", loss)
 
-            self.__loss_summary = tf.summary.scalar("loss", self.__loss)
-
-            optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
-            self.__train = optimizer.minimize(self.__loss)
+        optimizer = tf.train.AdamOptimizer(learning_rate=1e-4)
+        train = optimizer.minimize(loss)
+        return X, log_probs, actions, advantages, loss, loss_summary, train
 
     def act(self, observation):
-        sampled_prob = self.__sess.run(self.__probs, {self.__X: observation})
-        ## todo breaking abstraction here :)
+        sampled_prob = self._sess.run(self._log_probs, {self._X: observation})
+        # todo breaking abstraction here :)
         action = 2 if np.random.uniform() < sampled_prob[0][0] else 3  # roll the dice!
         return action
 
     def train(self, observations, actions, rewards):
-        ## todo normalize shouldn't be here
+        # todo normalize shouldn't be here
         rewards -= np.mean(rewards)
         rewards /= np.std(rewards)
 
-        feed_dict = { self.__X: observations,
-                self.__advantages: rewards,
-                      self.__actions: actions}
-        return self.__sess.run([self.__train, self.__loss_summary, self.__loss, self.__log_probs, self.__hoge, self.__probs, tf.shape(self.__advantages), tf.shape(self.__log_probs), tf.shape(self.__hoge)], feed_dict=feed_dict)
+        feed_dict = {
+            self._X: observations,
+            self._advantages: rewards,
+            self._actions: actions
+        }
+        return self._sess.run([self._train, self._loss_summary, self._loss, self._log_probs], feed_dict=feed_dict)
 
 
 
@@ -172,8 +173,7 @@ def main():
                 batch_observations.extend(observations)
                 batch_rewards.extend(processed_rewards)
                 batch_actions.extend(actions)
-            _, summary, loss, logprogs, hoge, probs, a_shape, p_shape, h_shape = model.train(np.vstack(batch_observations).T, np.vstack(actions).T, np.vstack(batch_rewards).T)
-            print(a_shape, p_shape, h_shape)
+            _, summary, loss, logprogs = model.train(np.vstack(batch_observations).T, np.vstack(actions).T, np.vstack(batch_rewards).T)
             if j % 5 == 0:
                 print("saved model")
                 saver.save(sess, './model/model.ckpt', global_step=j)
